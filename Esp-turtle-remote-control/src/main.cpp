aaf75fc151886e23028425204a8e8f6047cece58
alex.anipchenko <alex.anipchenko@gmail.com>
2020-01-20 16:09:16 +0200

    Create server

--- Esp-turtle-remote-control/src/main.cpp
+++ Esp-turtle-remote-control/src/main.cpp
@@ -3,54 +3,54 @@
 #include <ESP8266WebServer.h>
 #include "LcdKeypadShield.h"
 #include "WemosD1R1Pins.h"
+#include "Commands.h"
 
 //Create oobject by board type
 LcdKeypadShield lcd = LcdKeypadShield::createByBoaard(WemosD1R1);
 
+//Wi-Fi data
 const char* ssid = "";
-
 const char* password = "";
-
+//Server object and port
 ESP8266WebServer server(80);
-
 
 void handleRoot() 
 {
-  server.send(200, "text/plain", "Hello World!");
+  String command = NONE;
+
+  switch (lcd.getButton())
+    {
+    case Up:
+      command = UP;
+      break;
+
+    case Down:
+      command = DOWN;
+      break;
+
+    case Left:
+      command = LEFT;
+      break;
+    
+    case Right:
+      command = RIGHT;
+      break;
+    }
+
+  server.send(200, "text/plain", command);
 }
 
 void handleNotFound()
 {
-  String message = "File Not Found\n\n";
-
-  message += "URI: ";
-
-  message += server.uri();
-
-  message += "\nMethod: ";
-
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-
-  message += "\nArguments: ";
-
-  message += server.args();
-
-  message += "\n";
-
-  for (uint8_t i=0; i<server.args(); i++)
-  {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-
-  server.send(404, "text/plain", message);
+  server.send(404, "text/plain", NONE);
 }
-
-
 
 void setup(void)
 {
   //set lcd size
   lcd.begin(16,2);
+
+  lcd.userButtonsAutoCorrect();
 
   WiFi.begin(ssid, password);
 
@@ -77,11 +77,6 @@ void setup(void)
   lcd.print(ip.toString().c_str());
 
   server.on("/", handleRoot);
-
-  server.on("/inline", []()
-  {
-    server.send(200, "text/plain", "Inline method");
-  });
 
   server.onNotFound(handleNotFound);
 
